b191128a0b1f5f811480382408d7bc10eb847f43
Javi Guerra <javier.guerra1001@gmail.com>
2023-12-24 15:29:13 -0800

    feat(notifications): added toast notifcation container in application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,44 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { shallowEqual, useSelector, useDispatch } from 'react-redux';
+import { shallowEqual, useSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import RegistrationRoutes from './features/registration/Registration.routes';
 import GameRoutes from './features/game/Game.routes';
 import DashboardRoutes from './features/dashboard/Dashboard.routes';
 import NotFound from './common/components/NotFound';
 import ProtectedRoutes from './common/components/ProtectedRoutes';
-import { socket } from './common/service/socket';
 import { RootState } from './store';
-import { setConnected } from './store/UserSlice';
 
 function App() {
-  const dispatch = useDispatch();
   const { loggedIn } = useSelector(
     (state: RootState) => state.user,
     shallowEqual,
   );
-  useEffect(() => {
-    // only connect once in entire app when the app loads
-    socket.on('connect', () => {
-      dispatch(setConnected);
-    });
-    return () => {
-      socket.off('connect');
-    };
-  }, []);
   return (
-    <Routes>
-      <Route index element={<Navigate to="/home" />} />
-      <Route path="/home/*" element={<RegistrationRoutes />} />
-      <Route
-        path="/game/*"
-        element={
-          <ProtectedRoutes loggedIn={loggedIn}>
-            <GameRoutes />
-          </ProtectedRoutes>
-        }
-      />
-      <Route
-        path="/dashboard/*"
-        element={
-          <ProtectedRoutes loggedIn={loggedIn}>
-            <DashboardRoutes />
-          </ProtectedRoutes>
-        }
-      />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route index element={<Navigate to="/home" />} />
+        <Route path="/home/*" element={<RegistrationRoutes />} />
+        <Route
+          path="/game/*"
+          element={
+            <ProtectedRoutes loggedIn={loggedIn}>
+              <GameRoutes />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/dashboard/*"
+          element={
+            <ProtectedRoutes loggedIn={loggedIn}>
+              <DashboardRoutes />
+            </ProtectedRoutes>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
