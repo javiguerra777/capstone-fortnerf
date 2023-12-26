a976fbc569e29df63eb390e0a7e8a87141e6af9b
Javi Guerra <javier.guerra1001@gmail.com>
2023-12-25 22:38:02 -0800

    feat(messages): added styling to placeholder messages, added functionality to update search params to search messages and to change active room id

---
+++ client/src/features/direct-messages/service/Messages.service.ts
@@ -0,0 +1,46 @@
+export const MessagesPlaceholder = [
+  {
+    id: '1',
+    from: 'John Doe',
+    message: 'Hello, how are you?',
+    roomId: '1',
+    time: '12:00',
+  },
+  {
+    id: '2',
+    from: 'Jane Doe',
+    message: "Hi, what's up?",
+    roomId: '2',
+    time: '12:05',
+  },
+  {
+    id: '3',
+    from: 'Alice',
+    message: 'Good morning!',
+    roomId: '3',
+    time: '08:00',
+  },
+  {
+    id: '4',
+    from: 'Bob',
+    message: 'Good evening!',
+    roomId: '1',
+    time: '18:00',
+  },
+  {
+    id: '5',
+    from: 'Charlie',
+    message: "How's it going?",
+    roomId: '2',
+    time: '14:00',
+  },
+  {
+    id: '6',
+    from: 'Dave',
+    message: 'Nice to meet you!',
+    roomId: '3',
+    time: '16:00',
+  },
+];
+
+export default {};
