b4701c53dc9637cc93cfa1a4b0c3086c84b7d827
Javi Guerra <javier.guerra1001@gmail.com>
2023-12-26 23:24:39 -0800

    feat(add dm): when user sends direct message, it updates the room messages and the list of rooms and messages to show the latest stuff on their end

--- client/src/common/api/DirectMessagesApi.js
+++ client/src/common/api/DirectMessagesApi.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 import {
   createApi,
   fetchBaseQuery,
@@ -32,6 +34,41 @@ const DirectMessagesApi = createApi({
         body: { message, roomId, recipients },
       }),
       transformResponse: (response) => response.data,
+      onQueryStarted: async (
+        { roomId },
+        { dispatch, queryFulfilled },
+      ) => {
+        let patchResultRoomMessages;
+        let patchResultDirectMessages;
+        try {
+          const result = await queryFulfilled;
+          patchResultRoomMessages = dispatch(
+            DirectMessagesApi.util.updateQueryData(
+              'getDirectMessagesByRoomId',
+              roomId,
+              (draft) => {
+                draft.messages.push(result.data);
+              },
+            ),
+          );
+          patchResultDirectMessages = dispatch(
+            DirectMessagesApi.util.updateQueryData(
+              'getDirectMessages',
+              '',
+              (draft) => {
+                draft[roomId].messages.unshift(result.data);
+              },
+            ),
+          );
+        } catch {
+          if (patchResultRoomMessages) {
+            patchResultRoomMessages.undo();
+          }
+          if (patchResultDirectMessages) {
+            patchResultDirectMessages.undo();
+          }
+        }
+      },
     }),
   }),
 });
